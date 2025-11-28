b52d36f4bcc7a34ab9b203dc07e6fee81e9c4ec5
DavidPARK0417 <decidepyb@gmail.com>
2025-11-28 16:58:02 +0900

    fix(wholesaler): 도매 페이지 배경색 추가
    - 도매 페이지의 메인 컨텐츠 영역에 배경색을 추가하여 시각적 구분을 개선하였습니다.
    - `layout.tsx` 파일에서 `<main>` 태그에 `bg-gray-50` 클래스를 추가하였습니다.

--- app/wholesaler/layout.tsx
+++ app/wholesaler/layout.tsx
@@ -57,7 +57,7 @@ export default async function WholesalerLayout({
           <WholesalerHeader role={profile.role} />
 
           {/* 메인 컨텐츠 */}
-          <main className="flex-1 p-6">{children}</main>
+          <main className="flex-1 p-6 bg-gray-50">{children}</main>
         </div>
       </div>
     );
@@ -124,7 +124,7 @@ export default async function WholesalerLayout({
         <WholesalerHeader role={profile.role} />
 
         {/* 메인 컨텐츠 */}
-        <main className="flex-1 p-6">{children}</main>
+        <main className="flex-1 p-6 bg-gray-50">{children}</main>
       </div>
     </div>
   );
